b44cf91a58c60d0816d65f0ec7fb1e1bd75a148b
theeltahir <theeltahir@gmail.com>
2015-04-21 16:12:45 -0400

    Updates to pets views and controllers

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,5 @@
 class PetsController < ApplicationController
+    before_action :find_pet, only: [:show, :edit, :update, :destroy]
 
     def index
         @pets = Pet.all()
@@ -7,6 +8,51 @@ class PetsController < ApplicationController
 
     def show
         @pet = Pet.find(params[:id])
+    end
+
+    def edit
+        @pet = Pet.find(params[:id])
+    end
+
+    def new
+        @pet = Pet.new()
+    end
+
+    def create
+        @shelter = Shelter.find(params[:shelter_id])
+        @pet = @shelter.pets.new(pet_params)
+
+        if @pet.save?
+            redirect_to @pet
+        else
+            show :new
+        end
+    end
+
+    def update
+
+        if (@pet.update(pet_params))
+            redirect_to @pet
+        else
+            show :edit
+        end
+
+
+    end
+
+
+
+    private
+
+    def pet_params
+        params.require(:pet).permit(:name, :breed,  :sex, :color, :size, :age, :energy, :kid_friendly, :bio, :picture_url)
+    end
+    def find_pet
+        if params[:shelter_id]
+            @pet = Pet.find(params[:id])
+        else
+            @pet = Pet.find_by(shelter_id: params[:shelter_id], id: params[:id])
+        end
     end
 
 end
