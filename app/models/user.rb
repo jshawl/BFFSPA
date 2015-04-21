8397f9e3cf8df984ad3c358d43d62cda10900dfd
Karen Ochsenreiter <karen.e.och@gmail.com>
2015-04-21 16:34:31 -0400

    Set up Devise functionality throughout app/db; integrated Favorites class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ActiveRecord::Base
-  has_many :favorites
   has_one :user_profile
+  has_many :favorites
+  has_many :pets, through: :favorites
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
        :recoverable, :rememberable, :trackable, :validatable
 end
+
+
+
+
+
+
