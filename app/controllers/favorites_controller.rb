2f91877d1e63b51da0e40b64a790caa797b77a79
theeltahir <theeltahir@gmail.com>
2015-04-23 10:11:56 -0400

    favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,20 +1,19 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
   # before any "favoriting" action happens, the above line will authenticate the user.
-  
+
 
     def index
      @favorites = current_user.favorites
-     # 'current_user' helper method retrieves the user (object) that was authenticated
     end
 
-    # def create
-    # @favorite = current_user.favorites.new(favorite_params)      
-    #   if @favorite.save
-    #       redirect_to @favorite                 ### auto-redirect to page of all user favorites??
-    #   else render 'new'                         ## render new, or just allow user to continue scrolling through pets?                 
-    #   end
-    # end
+    def create
+
+      @pet = Pet.find(params[:pet][:id])
+      @favorite  = Favorite.find_or_create_by(user_id: current_user.id, pet_id: @pet.id)
+      #current_user.favorites.create(pet_id: @pet.id)
+      redirect_to @pet
+    end
 
 end
 
