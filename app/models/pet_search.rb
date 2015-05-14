339b14a821ad40312bfdad47345c21d14841d0eb
theeltahir <theeltahir@gmail.com>
2015-05-14 12:25:23 -0400

    more updates

--- app/models/pet_search.rb
+++ app/models/pet_search.rb
@@ -1,10 +1,16 @@
 class PetSearch
+
     def initialize(params)
-        query = filter_params(params)
-        @pets =   Pet.joins(:shelter).where(query).order('random()').limit(5)
+        @query = filter_params(params)
+
     end
 
     def pets
+        @pets =   Pet.where(@query).order('random()').limit(5)
+        if @pets.count == 0
+            #will fix the algorthem to pop items at the right order for maximum results
+            @pets =  Pet.order('random()').limit(5)
+        end
         @pets
     end
 
@@ -13,6 +19,5 @@ class PetSearch
     def filter_params(params)
         results = params.slice(:pet_type, :breed, :sex, :color, :size, :age, :energy, :kid_friendly)
         return results.compact
-
     end
 end
